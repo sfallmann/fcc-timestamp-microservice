b3c77e0a8bd850bb1024926f1df6f086bb4fabf5
Sean Fallmann <jorundr@hotmail.com>
2017-04-29 19:45:59 -0400

    Add route GET /:dateStr and return json with unix\natural times if valid or null if not

--- server.js
+++ server.js
@@ -4,9 +4,34 @@ const moment = require('moment');
 const app = express();
 const port = 3000 || process.env.PORT;
 
-app.get('/:date', function(req, res){
-  const date = req.params.date;
-  res.send(`You sent ${date}`);
+
+app.get('/favicon.ico', function(req, res) {
+  res.sendStatus(204);
+});
+
+app.get('/:dateStr', function(req, res) {
+  let date;
+  const dateObj = {
+    unix: null,
+    natural: null
+  };
+
+  res.set({
+    'Content-Type': 'application/json'
+  });
+
+  if (Number(req.params.dateStr).toString() === 'NaN') {
+    date = req.params.dateStr;
+  } else {
+    date = Number(req.params.dateStr);
+  }
+
+  if (moment(date).isValid()) {
+    dateObj.unix = moment(date).unix();
+    dateObj.natural = moment(date).format('MMMM Do, YYYY');
+  }
+
+  res.json(dateObj);
 });
 
 app.listen(port);
