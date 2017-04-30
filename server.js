87b358f0478b03de73e795fbc77c80503a6d2029
Sean Fallmann <jorundr@hotmail.com>
2017-04-30 07:34:32 -0400

    Add rate limiting of requests to 100\hour

--- server.js
+++ server.js
@@ -2,12 +2,22 @@
 
 const express = require('express');
 const moment = require('moment');
+const RateLimit = require('express-rate-limit');
 const {timeInMs} = require('./helper');
 
 const app = express();
 const port =  process.env.PORT || 3000;
 
+app.enable('trust proxy');
 app.use(express.static('public'));
+app.disable('x-powered-by');
+
+const apiLimiter = new RateLimit({
+  // allow 100 requests/hour
+  windowMs: 60 * 60 * 1000,
+  max: 100,
+  delayMs: 0
+});
 
 // A good solution found here:
 // http://stackoverflow.com/questions/35408729/express-js-prevent-get-favicon-ico
@@ -15,9 +25,13 @@ app.get('/favicon.ico', function(req, res) {
   res.sendStatus(204);
 });
 
-app.get('/:date', function(req, res) {
+app.get('/:date', apiLimiter, function(req, res) {
   let dateObj;
   const time = timeInMs(req.params.date);
+  res.set({
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Origin': '*'
+  });
 
   if (time === null) {
     dateObj = {
@@ -33,13 +47,7 @@ app.get('/:date', function(req, res) {
     };
   }
 
-  // if the request is an ajax request or the Accept header is json
-  // return dateObj as json, otherwise return it as text\html
-  if (req.xhr || req.headers.accept.indexOf('json') > -1) {
-    res.json(dateObj);
-  } else {
-    res.send(JSON.stringify(dateObj));
-  }
+  res.json(dateObj);
 
 });
 
