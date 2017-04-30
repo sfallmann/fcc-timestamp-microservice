6d02c773c3002b2ae9e1343eb866c5754de0d859
Sean Fallmann <jorundr@hotmail.com>
2017-04-29 20:48:44 -0400

    Fix issue causing moment deprecation warning and add index.html with API info
    Added page with the requirements for the project
    Moment.js has stricter string format rules than the native Date type.  To prevent the deprecation message and
    avoid potential future problems, I use the string to create a Date obj and pass that into moment for formatting.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const moment = require('moment');
 
@@ -5,6 +7,10 @@ const app = express();
 const port = 3000 || process.env.PORT;
 
 
+app.use(express.static('public'));
+
+// A good solution found here:
+// http://stackoverflow.com/questions/35408729/express-js-prevent-get-favicon-ico
 app.get('/favicon.ico', function(req, res) {
   res.sendStatus(204);
 });
@@ -20,13 +26,23 @@ app.get('/:dateStr', function(req, res) {
     'Content-Type': 'application/json'
   });
 
+  // check if the value sent can be coerced to a number
+  // if it can't, use the string sent
+  // otherwise coerce to a number
   if (Number(req.params.dateStr).toString() === 'NaN') {
     date = req.params.dateStr;
   } else {
     date = Number(req.params.dateStr);
   }
 
+  // set date to a Date object
+
+  date = new Date(date);
+
+  // check the validity of date
+  // and format accordingly
   if (moment(date).isValid()) {
+    // same as Date.getTime(), decided to use moment for consistency
     dateObj.unix = moment(date).unix();
     dateObj.natural = moment(date).format('MMMM Do, YYYY');
   }
